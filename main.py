5f89a1630b8326842cdb5c23c90561b72f644036
Rangga1708 <ranggajaya.c@gmail.com>
2022-08-13 10:45:44 +0700

    Edit main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import streamlit as st
 import pandas as pd
 
+#data = pd.read_csv("https://raw.githubusercontent.com/Rangga1708/Food-Recommender/main/Food%20Taste.csv")
+
+st.header("Food Recommender")
+# st.session_state["name"] = st.text_input("Food's tastes you want: ")
+
+# st.session_state.name
