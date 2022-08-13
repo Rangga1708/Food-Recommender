0064ad9bc0375ce10f07ec04055b3b89fd41c734
Rangga1708 <ranggajaya.c@gmail.com>
2022-08-13 10:46:27 +0700

    Edit main.py

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ import pandas as pd
 #data = pd.read_csv("https://raw.githubusercontent.com/Rangga1708/Food-Recommender/main/Food%20Taste.csv")
 
 st.header("Food Recommender")
-# st.session_state["name"] = st.text_input("Food's tastes you want: ")
+st.text_input("Food's tastes you want: ", key="name")
 
-# st.session_state.name
+st.write(f"You want {st.session_state.name}")
