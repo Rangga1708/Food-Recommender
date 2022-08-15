7a31734841086803c0431be782dde25e2b03d1c4
Rangga1708 <ranggajaya.c@gmail.com>
2022-08-15 15:49:31 +0700

    Edit main.py, show dataframe

--- main.py
+++ main.py
@@ -33,5 +33,6 @@ data["similarity"] = data["taste_vector"].map(similarity)
 filtered_data = data[data["similarity"] > 0]
 filtered_data = filtered_data.sort_values(by = "similarity", ascending = False)
 filtered_data = filtered_data.reset_index(drop = True)
+filtered_data = filtered_data.drop(columns = ["taste_vector", "similarity"])
 
 st.dataframe(filtered_data)
