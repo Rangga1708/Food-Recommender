ce332f93ea908e8bf036361ae28e5831607e3b38
Rangga1708 <ranggajaya.c@gmail.com>
2022-08-15 11:01:20 +0700

    Edit main.py

--- main.py
+++ main.py
@@ -20,24 +20,25 @@ def similarity(x):
     
     return np.dot(taste, user_taste)
 
-def check(tastes):
-    result = []
-    
-    for taste in tastes:
-        st.write(taste)
-        taste_vector = get_taste(taste)
-        result.append(similarity(taste_vector))
-    
-    return result
+#def check(tastes):
+#    result = []
+#    
+#    for taste in tastes:
+#        st.write(taste)
+#        taste_vector = get_taste(taste)
+#        result.append(similarity(taste_vector))
+#    
+#    return result
 
 st.header("Food Recommender")
 st.text_input("Food's tastes you want: ", key="name")
 user_taste_vector = get_taste(st.session_state.name)
 
 data = pd.read_csv("https://raw.githubusercontent.com/Rangga1708/Food-Recommender/main/Food%20Taste.csv")
-data["check"] = check(data["taste_name"].to_list())
-filtered_data = data[data["check"] > 0]
-filtered_data = filtered_data.sort_values(by = "check", ascending = False)
+data["taste_vector"] = data["taste"].map(get_taste)
+data["similarity"] = data["taste_vector"].map(similarity)
+filtered_data = data[data["similarity"] > 0]
+filtered_data = filtered_data.sort_values(by = "similarity", ascending = False)
 filtered_data = filtered_data.reset_index(drop = True)
 
 for i in range(len(filtered_data)):
